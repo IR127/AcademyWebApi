10f7deb22a0b780d3a866a3a248fd96c56b824c5
Idrees Rabani <idrees.rabani@asos.com>
2018-09-28 15:05:26 +0100

    Created Interface for data source and model for a task

---
+++ ToDoList/Interfaces/IDataStore.cs
@@ -0,0 +1,11 @@
+﻿namespace ToDoList.Interfaces
+{
+    using System.Collections.Generic;
+
+    public interface IDataStore
+    {
+        IEnumerable<UserTask> Read(int userId);
+
+        bool Create(UserTask task);
+    }
+}
---
+++ ToDoList/Models/UserTask.cs
@@ -0,0 +1,17 @@
+﻿namespace ToDoList.Models
+{
+    using System;
+
+    public class UserTask
+    {
+        public int UserId { get; set; }
+
+        public int TaskId { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime DueBy { get; set; }
+
+        public bool Completed { get; set; }
+    }
+}
